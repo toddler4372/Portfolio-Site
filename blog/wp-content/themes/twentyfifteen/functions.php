ba3a6a75b8582ba7c060bbab87c89eb62c206f0b
toddler4372 <toddlerwilliams@gmail.com>
2015-04-10 13:50:41 -0700

    Added SVG support for Wordpress

--- blog/wp-content/themes/twentyfifteen/functions.php
+++ blog/wp-content/themes/twentyfifteen/functions.php
@@ -329,3 +329,18 @@ require get_template_directory() . '/inc/template-tags.php';
  * @since Twenty Fifteen 1.0
  */
 require get_template_directory() . '/inc/customizer.php';
+
+//  Remove warning for SVGs
+add_filter('upload_mimes', 'custom_upload_mimes');
+
+function custom_upload_mimes ( $existing_mimes=array() ) {
+
+	// add the file extension to the array
+
+	$existing_mimes['svg'] = 'mime/type';
+
+        // call the modified list of extensions
+
+	return $existing_mimes;
+
+}
